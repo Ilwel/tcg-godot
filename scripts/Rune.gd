419c43d64ea657e09466261ba8454aa1270c9bf0
iueu <ilwelisaac@hotmail.com>
2025-12-01 13:13:34 -0300

    feat: gems

--- scripts/Rune.gd
+++ scripts/Rune.gd
@@ -4,32 +4,59 @@ extends Control
 enum RuneType { FIRE, WATER, EARTH, AIR, DARK, LIGHT }
 @export var rune_type: RuneType
 @onready var sprite: Sprite2D = $RuneSprite
-@export var enabled:bool = false
+@onready var bright_animation: AnimatedSprite2D = $BrightAnimation
+@export var enabled:bool = true
+@export var ready_to_use = false
 
-func get_rune_sprite():
+func get_rune_color() -> Color:
 	if rune_type == RuneType.FIRE:
-		return preload("res://assets/runes/FireRune.png")
+		return Color("f22760ff")
 	elif rune_type == RuneType.WATER:
-		return preload("res://assets/runes/WaterRune.png")
+		return Color("005debff")
 	elif rune_type == RuneType.EARTH:
-		return preload("res://assets/runes/EarthRune.png")
+		return Color("53fa43ff")
 	elif rune_type == RuneType.AIR:
-		return preload("res://assets/runes/AirRune.png")
+		return Color("63cbe9ff")
 	elif rune_type == RuneType.DARK:
-		return preload("res://assets/runes/DarkRune.png")
+		return Color("#181825")
 	elif rune_type == RuneType.LIGHT:
-		return preload("res://assets/runes/LightRune.png")
+		return Color("#ffffff")
+	else:
+		return Color("ffffff")
+		
+func _set_intensity(value):
+	var c = sprite.modulate
+	c.r = value
+	c.g = value
+	c.b = value
+	modulate = c
+	
+func set_glow(state: bool):
+	if bright_animation:
+		bright_animation.visible = state
+	if state:
+		if rune_type == RuneType.DARK:
+			_set_intensity(4.0)
+		else:
+			_set_intensity(2.0)
+		bright_animation.play()
+	else:
+		_set_intensity(1.0)
+		bright_animation.stop()
+		
+	
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	sprite.texture = get_rune_sprite()
-		
+	sprite.modulate = get_rune_color()
+	set_glow(ready_to_use)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	sprite.texture = get_rune_sprite()
+	sprite.modulate = get_rune_color()
+	set_glow(ready_to_use)
 	if !enabled:
-		sprite.modulate.a = 0.5
+		sprite.modulate.a = 0.2
 	else:
 		sprite.modulate.a = 1
