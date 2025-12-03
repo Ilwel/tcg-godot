641545c8e494ad7d0712eacb3c50fe8fe69cc743
iueu <ilwelisaac@hotmail.com>
2025-12-03 16:50:20 -0300

    feat: new cards

--- scenes/CoinWindow.gd
+++ scenes/CoinWindow.gd
@@ -5,7 +5,6 @@ extends VBoxContainer
 @onready var coin_decision = $CoinDecision
 @onready var label = $Label
 @onready var throw_coin = $HBoxContainer/Coin
-@onready var result_lbl = $ResultLbl
 @onready var modal_screen = $".."
 
 # Called when the node enters the scene tree for the first time.
@@ -31,6 +30,7 @@ func on_coin_click(face_parm):
 	await get_tree().create_timer(1.0).timeout
 	modal_screen.visible = false
 	Match.match_game["current_game_phase"] = Match.GamePhaseType.Init
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
